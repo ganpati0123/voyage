9cffd2f658e3e668e7de9dd7018b596eaa9f8ad5
Ganpati Raj <ganpatiraj716@gmail.com>
2026-08-12 08:57:42 +0530

    Remove unused About import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/About';
 import Tracks from './components/Tracks';
 import Timeline from './components/Timeline';
 import Prizes from './components/Prizes';
 import Sponsors from './components/Sponsors';
-import Guidelines from './components/Guidelines';
 import GridAbout from './components/GridAbout';
+import Guidelines from './components/Guidelines';
 import FAQ from './components/FAQ';
 import Register from './components/Register';
 import Contact from './components/Contact';
@@ -17,12 +16,11 @@ function App() {
     <div style={{ minHeight: '100vh', background: 'var(--abyss)' }}>
       <Navbar />
       <Hero />
-      <About />
       <Tracks />
-      <Timeline />
       <Prizes />
-      <Sponsors />
       <Guidelines />
+      <Timeline />
+      <Sponsors />
       <GridAbout />
       <FAQ />
       <Register />
